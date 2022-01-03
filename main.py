c1a7ef813daf7a0f0a5013ff833d679c3e3f12b8
Mirnawati <mw151476@gmail.com>
2022-01-03 18:05:25 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+phi = 3.14
+r = float(input("Masukkan panjang jari-jari lingkaran: "))
+
+luas = phi*(r*r)
+keliling = 2*phi*r
+
+print("Luas lingkaran adalah : "+ str(luas))
+print("Keliling lingkaran adalah : "+ str(keliling))
